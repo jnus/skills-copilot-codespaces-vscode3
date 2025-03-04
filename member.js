eb72052a8b742d9744fcf8a58f4c8f7f07153885
Jasper Nygaard <jasper.nygaard@solidify.dev>
2025-03-04 11:07:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  // Code here
+  var skills = ['JavaScript', 'React', 'Node.js', 'MongoDB', 'Express'];
+  for (var i = 0; i < skills.length; i++) {
+    console.log(skills[i]);
+  }
+}
+  
